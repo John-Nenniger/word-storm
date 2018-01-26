9e96a4df91a7a82494d4fb644c4e16459bd6e846
John Nenniger <JPNENNIGER@GMAIL.com>
2018-01-25 16:31:30 -0800

    making progress, need to randomize font-size now :

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,10 +1,23 @@
 
   console.log("stuffs loaded ")
+  const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
   const words = {"ethereal": "extremely delicate and light in a way that seems too perfect for this world", "lugubrious": "looking or sounding sad and dismal", "pernicious": "having a harmful effect, especially in a gradual or subtle way", "litigious": "unreasonably prone to go to law to settle disputes", "nefarious": "(typically of an action or activity) wicked or criminal"}
 
+  const random = function(max){
+    return Math.floor((Math.random() * max) + 1)
+  }
+
   for (const [key, value] of Object.entries(words)){
-    let hNumber = Math.floor(Math.random() * 6 + 1);
-    let word = document.createElement(`h${hNumber}`)
-    word.innerHTML = key
-    document.getElementById("body").appendChild(word)
+    // let hNumber = random(6);
+    let word = document.createElement("p");
+    word.innerHTML = key;
+    word.className += "word";
+    word.style.position = "absolute";
+    console.log()
+    document.getElementById("body").appendChild(word);
+    let wordHeight = word.offsetHeight;
+    let wordWidth = word.offsetWidth;
+    word.style.top = `${random(windowHeight - wordHeight)}px`;
+    word.style.right = `${random(windowWidth - wordWidth)}px`;
   }
