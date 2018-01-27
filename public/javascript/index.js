6cd7654c0ac000df42ba66b07375d3dc43e4620c
John Nenniger <JPNENNIGER@GMAIL.com>
2018-01-26 22:15:33 -0800

    added more words

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,7 +2,17 @@
   console.log("stuffs loaded ")
   const windowHeight = window.innerHeight;
   const windowWidth = window.innerWidth;
-  const words = {"ethereal": "extremely delicate and light in a way that seems too perfect for this world", "lugubrious": "looking or sounding sad and dismal", "pernicious": "having a harmful effect, especially in a gradual or subtle way", "litigious": "unreasonably prone to go to law to settle disputes", "nefarious": "(typically of an action or activity) wicked or criminal"}
+  const words = {"Ethereal": "extremely delicate and light in a way that seems too perfect for this world",
+                 "Lugubrious": "looking or sounding sad and dismal",
+                 "Pernicious": "having a harmful effect, especially in a gradual or subtle way",
+                 "Litigious": "unreasonably prone to go to law to settle disputes",
+                 "Nefarious": "(typically of an action or activity) wicked or criminal",
+                 "Decathect": "to withdraw one’s feelings of attachment from (a person, idea, or object), as in anticipation of a future loss",
+                 "Nycotophilia": "love of darkness or night. Finding relaxation or comfort in the darkness",
+                 "Agathist": "a person who believes that all things tend towards ultimate good",
+                 "Retroherence" :"the tendency of clusters of events to be logically connectable after they’ve happened",
+                 "Nemorian": "an inhabitant of - or pertaining to - forests, woodland, or groves"
+               }
 
   const random = function(max){
     return Math.floor((Math.random() * max) + 1)
